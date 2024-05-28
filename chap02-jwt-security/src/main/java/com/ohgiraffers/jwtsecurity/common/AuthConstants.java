d90a98e997bfbee7f72888f4abd5937c46ed6990
lmcwul <cksdnf90@gmail.com>
2024-05-28 09:36:45 +0900

    ## wrong commit fix

--- chap02-jwt-security/src/main/java/com/ohgiraffers/jwtsecurity/common/AuthConstants.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.jwtsecurity.common;
-
-/* Jwt 관련 상수 필드 */
-public class AuthConstants {
-
-    public static final String AUTH_HEADER = "Authorization";
-
-    public static final String TOKEN_TYPE = "BEARER";
-}
